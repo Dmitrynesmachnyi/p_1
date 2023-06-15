97f35f4a5cbe77d238021cba938217db9ec385c0
Дмитрий <113@mail.ru>
2023-06-15 20:33:14 +0300

    initial

---
+++ 4.1.py
@@ -0,0 +1,28 @@
+'''def strcounter(s): # O ( N*M)
+    for sym in set(s) :
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+strcounter('adcnra')
+
+def strcounter(s): # O (N**2)
+    for sym in s :
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+strcounter('adcnra')'''
+
+def strcounter(s):#O(N)
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym,0) + 1
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+strcounter('adcnra')
+
+
